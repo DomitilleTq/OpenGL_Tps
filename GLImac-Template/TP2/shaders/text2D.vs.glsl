094f9cdd41cd5633aed1a4e3689ec22aeb63d4b6
DomitilleTq <122257063+DomitilleTq@users.noreply.github.com>
2023-01-12 10:48:56 +0100

    Update text2D.vs.glsl

--- GLImac-Template/TP2/shaders/text2D.vs.glsl
+++ GLImac-Template/TP2/shaders/text2D.vs.glsl
@@ -5,8 +5,13 @@ layout(location = 1) in vec2 aVertexTexture;
 
 out vec2 vVertexTexture;
 
+uniform mat3 uModelMatrix;
+
 void main() {
     vVertexTexture = aVertexTexture;
-    gl_Position = vec4(aVertexPosition, 0, 1);// z=0 et homogene w=1
+    
+  vec2 transformed = (vec3(aVertexPosition, 1)*uModelMatrix).xy;
+  gl_Position = vec4(transformed, 0, 1);
+    //gl_Position = vec4(aVertexPosition, 0, 1);// z=0 et homogene w=1
     
 }
