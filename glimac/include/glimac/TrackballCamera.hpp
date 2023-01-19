87af465388a38c9d38638ba09a17d05232b10a65
DomitilleTq <122257063+DomitilleTq@users.noreply.github.com>
2023-01-19 19:04:41 +0100

    Update TrackballCamera.hpp

--- glimac/include/glimac/TrackballCamera.hpp
+++ glimac/include/glimac/TrackballCamera.hpp
@@ -3,9 +3,7 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <string>
-#include "Image.hpp"
-#include "FilePath.hpp"
-#include "BBox.hpp"
+
 
 namespace glimac {
 
@@ -16,26 +14,27 @@ private:
 	float m_fAngleY;
 	
 public:
-    TrackballCamera():
+    TrackballCamera(){
     	m_fDistance=5;
     	m_fAngleX=0;
     	m_fAngleY=0;
     }
     
     void moveFront(float delta){
-    	m_fDistance=m_fDistance*delta;
+    
+    	m_fDistance=m_fDistance+delta;
     } //permettant d'avancer / reculer la caméra. Lorsque delta est positif la caméra doit avancer, sinon elle doit reculer.
 	void rotateLeft(float degrees){
-		m_fAngleX=m_fAngleX*degrees;
+		m_fAngleX=m_fAngleX+degrees;
 	} //permettant de tourner latéralement autour du centre de vision.
 	void rotateUp(float degrees){
-		m_fAngleY=m_fAngleY*degrees;
+		m_fAngleY=m_fAngleY+degrees;
 	}//permettant de tourner verticalement autour du centre de vision.
 	
 	glm::mat4 getViewMatrix() const{		
 		glm::mat4 MatriceFinal = glm::translate(glm::mat4(1), glm::vec3(0, 0, -m_fDistance)); // Translation
-		MatriceFinal = glm::rotate(MVMatrix, windowManager.getTime(), glm::vec3(0, glm::radians(m_fAngleX), 0)); // Translation * Rotation
-		MatriceFinal = glm::rotate(MVMatrix, windowManager.getTime(), glm::vec3(0, glm::radians(m_fAngleY), 0)); // Translation * Rotation * Rotation
+		MatriceFinal = glm::rotate(MatriceFinal, glm::radians(m_fAngleX), glm::vec3(0, 1, 0)); // Translation * Rotation
+		MatriceFinal = glm::rotate(MatriceFinal, glm::radians(m_fAngleY), glm::vec3(1, 0, 0)); // Translation * Rotation * Rotation
 		return MatriceFinal;
 	}
 	
